a1bc184eb8361328e9e0008fd789d4aad6a2d781
aliashafi <alia_shafi@alumni.brown.edu>
2019-09-10 16:47:21 -0700

    try 4

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import SignupContainer from './session/Signup_Container';
-import LoginContainer from './session/Login_Container';
+import SignupContainer from './session/signup_container';
+import LoginContainer from './session/login_container';
 import SplashPage from './splash_page'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import Feed from './feed/Feed'
+import Feed from './feed/feed'
 
 
 export default () => (
