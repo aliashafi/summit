27ae97f6bd0fb62d679f30be6e040b1a2db43af6
aliashafi <alia_shafi@alumni.brown.edu>
2019-09-10 17:12:42 -0700

    sql to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_10_162557) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "activity_type", null: false
