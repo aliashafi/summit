55c3794ca7a1606772f7462c486a7cf62149c856
aliashafi <alia_shafi@alumni.brown.edu>
2019-09-15 14:08:57 -0700

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -181,6 +181,7 @@ ActiveRecord::Base.transaction do
 
     file_names[2..-1].each do |file|
         next if file == ".DS_Store"
+        next if file == "."
         r1 = makeRoute("#{Rails.root}/db/gpx_routes/#{file}", u1.id)
         Kudo.create!(user_id: u2.id, activity_id: r1.id)
         Kudo.create!(user_id: u3.id, activity_id: r1.id)
@@ -193,6 +194,7 @@ ActiveRecord::Base.transaction do
      p "starting activity....all users"
     file_names_everyone[2..-1].each do |file|
         next if file == ".DS_Store"
+        next if file == "."
         r2 = makeRoute("#{Rails.root}/db/gpx_everyone/#{file}", u2.id)
         r3 = makeRoute("#{Rails.root}/db/gpx_everyone/#{file}", u3.id)
         r4 = makeRoute("#{Rails.root}/db/gpx_everyone/#{file}", u4.id)
